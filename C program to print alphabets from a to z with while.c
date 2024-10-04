90c05f88a47f122c48bc35ac180f20affb15c712
yousefammmar <123825108+yousefammmar@users.noreply.github.com>
2024-10-04 11:00:12 +0300

    Add files via upload

---
+++ C program to print alphabets from a to z with while.c
@@ -0,0 +1,11 @@
+//C program to print alphabets from a to z with while
+#include <stdio.h>
+int main(){
+    char i;
+    i='a';
+    while(i<='z'){
+    printf("%c\n",i);
+    i++;
+    }
+    return 0;
+}
